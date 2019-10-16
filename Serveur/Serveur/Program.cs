efa96ecd3576a7f02c903e19c93a6e4d4267b5b9
Hugo_Fer <hugo.fer@etud.u-picardie.fr>
2019-10-16 16:55:34 +0200

    création du .net core

---
+++ Serveur/Serveur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serveur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
