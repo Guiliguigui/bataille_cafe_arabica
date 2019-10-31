8a6133931c40edd879b9048e5de95d6c4d921688
Guillaume <gui.mairesse@gmail.com>
2019-10-31 14:16:59 +0100

    Implementation de la fonction Lecture qui lit et stocke les valeurs de la trame

---
+++ Decodage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Decodage
+{
+    public static int[,] Lecture(string trame)
+    {
+        int[,] tabValeursCarte = new int[10, 10];
+        string[] tabLignes = trame.Split('|');
+        for (int index1 = 0; index1 < 10; index1++)
+        {
+            string[] ligneSplit = tabLignes[index1].Split(':');
+            for (int index2 = 0; index2 < 10; index2++)
+                int.TryParse(ligneSplit[index2], out tabValeursCarte[index1, index2]);
+        }
+        return tabValeursCarte;
+    }
+}
