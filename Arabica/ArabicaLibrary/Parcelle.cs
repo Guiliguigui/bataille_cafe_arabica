32f672c79a135824e5622c77b3f55b89950263f8
Guillaume <gui.mairesse@gmail.com>
2020-03-05 15:56:26 +0100

    création des premières classe objet

---
+++ Arabica/ArabicaLibrary/Parcelle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicaLibrary
+{
+    public class Parcelle
+    {
+        private char nom;
+        private List<Case> cases;
+        private byte nbCase;
+        private byte nbCaseJouees;
+
+        public Parcelle(char nom, List<Case> cases, byte nbCase)
+        {
+            this.nom = nom;
+            this.cases = cases;
+            this.nbCase = nbCase;
+            nbCaseJouees = 0;
+        }
+
+        public char Nom { get => nom; set => nom = value; }
+        public List<Case> Cases { get => cases; set => cases = value; }
+        public byte NbCase { get => nbCase; set => nbCase = value; }
+        public byte NbCaseJouees { get => nbCaseJouees; set => nbCaseJouees = value; }
+
+    }
+}
---
+++ Arabica/ArabicaLibrary/case.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicaLibrary
+{
+    public class Case
+    {
+        private byte x, y;
+        private char parcelle;
+        private bool plantee;
+        private IA proprietaire;
+
+        public Case(byte x, byte y, char parcelle)
+        {
+            this.x = x;
+            this.y = y;
+            this.parcelle = parcelle;
+            plantee = false;
+            proprietaire = null;
+        }
+
+        public byte X { get => x; set => x = value; }
+        public byte Y { get => y; set => y = value; }
+        public char Parcelle { get => parcelle; set => parcelle = value; }
+        public bool Plantee { get => plantee; set => plantee = value; }
+        public IA Proprietaire { get => proprietaire; set => proprietaire = value; }
+
+        public void planter(IA proprietaire)
+        {
+            plantee = true;
+            this.proprietaire = proprietaire;
+        }
+    }
+}
